ffcd80a324db4a5eb57cc60e8b67e8ed086e780e
michaelboyer-Carleton <michaelboyer@cmail.carleton.ca>
2026-02-27 21:28:48 -0500

    Viewportbox bug fixed

--- untitled-computer-game/Character.gd
+++ untitled-computer-game/Character.gd
@@ -2,33 +2,38 @@ extends CharacterBody2D
 class_name Character
 
 #var team: int = 0
+static var characterCount = 0;
 
 @export
 var Health = 100
 
 @export
-var Speed: float = 150.0
-var Acceleration: float = 25.0
-var Decelleration: float = 25.0
+var speed: float = 150.0
+var acceleration: float = 25.0
+var deceleration: float = 25.0
 
 var move_dir: Vector2 = Vector2.ZERO
+
+func _ready() -> void:
+	self.get_node("SubViewportContainer").get_node("SubViewport").get_node("Camera3D").global_position.x += characterCount * 10
+	self.get_node("SubViewportContainer").get_node("SubViewport").get_node("ModelRoot").global_position.x += characterCount * 10
+	characterCount += 1
 
 func _physics_process(delta: float) -> void:
 	var velocity: Vector2 = self.velocity
 	var direction: Vector2 = move_dir
 
 	if direction != Vector2.ZERO:
-		velocity.x = move_toward(velocity.x, direction.x * Speed, Acceleration)
-		velocity.y = move_toward(velocity.y, direction.y * Speed, Acceleration)
+		velocity.x = move_toward(velocity.x, direction.x * speed, acceleration)
+		velocity.y = move_toward(velocity.y, direction.y * speed, acceleration)
 	else:
-		velocity.x = move_toward(velocity.x, 0.0, Decelleration)
-		velocity.y = move_toward(velocity.y, 0.0, Decelleration)
+		velocity.x = move_toward(velocity.x, 0.0, deceleration)
+		velocity.y = move_toward(velocity.y, 0.0, deceleration)
 
 	self.velocity = velocity
 	move_and_slide()
 
 func look_in_direction(dir: Vector2) -> void:
-	print(dir)
 	self.get_node("SubViewportContainer").get_node("SubViewport").get_node("ModelRoot").rotation.y = -global_position.direction_to(dir + self.global_position).angle()
 
 func set_move_dir(dir: Vector2) -> void:
---
+++ untitled-computer-game/test_map.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var enemyScene = preload("res://enemy.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print(Character.characterCount)
+	var newEnemy = enemyScene.instantiate()
+	get_node("Entities").add_child(newEnemy)
+	print(Character.characterCount)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
