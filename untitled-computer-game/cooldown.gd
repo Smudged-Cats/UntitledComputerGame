173a5bbc85610875bc23dcacf12dd3e22fb97881
Gitdynamics <replywithyes@gmail.com>
2026-03-06 22:39:46 -0500

    Cleaned some things up with the cooldown

--- untitled-computer-game/cooldown.gd
+++ untitled-computer-game/cooldown.gd
@@ -1,6 +1,12 @@
 extends Timer
+
+#Try and make cooldown more of a class that creates a "Cooldown"
+# object, as opposed to its current state of being a sort of
+# library with one funciton
 class_name Cooldown
 
+#CreateCooldown is an easier way to create a timer that
+# acts like a cooldown between attacks
 static func createCooldown(seconds:float) -> Timer:
 	var newTimer = Timer.new()
 	newTimer.wait_time = seconds
