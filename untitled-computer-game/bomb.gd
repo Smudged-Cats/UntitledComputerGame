43cde7e4e231a16d608d05c7fed1094b39a74a90
Hamburgerdotnet <hamburgerdotnet@gmail.com>
2026-03-06 21:46:39 -0500

    Bomb & Objective

---
+++ untitled-computer-game/bomb.gd
@@ -0,0 +1,45 @@
+extends Node2D
+
+@onready 
+var pickedUp = false
+
+@onready 
+var canPickup = false
+
+@onready 
+var onObjective = false
+
+@onready
+var isActivated = false
+
+@onready
+var playerCharacter = get_parent().get_node("Entities").get_node("Player").get_node("Character")
+
+func _process(delta: float) -> void:
+	if onObjective and pickedUp and Input.is_action_just_pressed("interact") and not isActivated:
+		activateBomb()
+	if pickedUp == true and not isActivated:
+		self.global_position = Vector2(playerCharacter.global_position)
+	if canPickup == true and not isActivated:
+		if Input.is_action_just_pressed("interact"):
+			if pickedUp == false:
+				pickedUp = true
+			elif pickedUp == true:
+				pickedUp = false
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	canPickup = true
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	canPickup = false
+	
+func activateBomb() -> void:
+	isActivated = true
+	pickedUp = false
+	canPickup = false
+	get_parent().get_node("ObjectivePoint").get_node("Timer").get_node("Label").visible = true
+	get_parent().get_node("ObjectivePoint").get_node("Timer").start()
+	get_parent().objectiveStarted = true
+	
--- untitled-computer-game/test_map.gd
+++ untitled-computer-game/test_map.gd
@@ -1,9 +1,25 @@
 extends Node2D
 
+var objectiveStarted = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	$ObjectivePoint/Timer/Label.visible = false
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var timeLeft = $ObjectivePoint/Timer.time_left
+	var minutesLeft: int = floor(timeLeft / 60.0)
+	var secondsLeft: int = fmod(timeLeft, 60.0)
+	var timeString: String = "%02d:%02d" % [minutesLeft, secondsLeft]
+	
+	
+	if timeLeft <= 0 and objectiveStarted:
+		print("Explosion")
+
+	$ObjectivePoint/Timer/Label.text = timeString
+func _on_objective_point_body_entered(body: Node2D) -> void:
+	$Bomb.onObjective = true
+
+func _on_objective_point_body_exited(body: Node2D) -> void:
+	$Bomb.onObjective = false
