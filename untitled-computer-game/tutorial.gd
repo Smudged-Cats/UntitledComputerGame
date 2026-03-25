6d4f7aa2d39b9523abd94a1e959e3e4474caa4ac
michaelboyer-Carleton <michaelboyer@cmail.carleton.ca>
2026-03-25 18:46:31 -0400

    Tutorial messages

--- untitled-computer-game/tutorial.gd
+++ untitled-computer-game/tutorial.gd
@@ -8,15 +8,21 @@ var currentMessage = null
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("Starting tutorial")
+	await get_tree().create_timer(1).timeout
 	currentMessage = add_message("Welcome to Untitled Computer Game!")
+	
+	await wait_until_message_is_null()
+		
+	currentMessage = add_message("what are you")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# Wait until message is removed
+func wait_until_message_is_null():
+	while (currentMessage != null):
+		await get_tree().create_timer(0.25).timeout
 
 func add_message(newMsg: String) -> TutorialMessage:
 	var msgScene = tutorialMessageScene.instantiate()
-	$Control.add_child(msgScene)
 	msgScene.message = newMsg
+	$Control.add_child(msgScene)
 	print(msgScene.message)
 	return msgScene
--- untitled-computer-game/tutorial_message.gd
+++ untitled-computer-game/tutorial_message.gd
@@ -1,6 +1,14 @@
 class_name TutorialMessage
 extends Label
 
+
+
+# Animation variables
+var easing_in = true
+var tEaseIn = 0
+static var curve = preload("res://resources/message_popup_curve.tres") # asset reuse
+
+# Flashing variables
 var text_alpha = 0 # The desired value we want the alpha channel of the prompt to be
 var flashing = true
 var flash_direction = 1
@@ -10,13 +18,24 @@ var message = "":
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	scale.x = 0
+	scale.y = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	# Listen for input
 	if Input.is_action_just_pressed("next message"):
 		remove_message()
+	
+	# Ease in
+	if easing_in:
+		tEaseIn += 2*delta
+		scale.x = curve.sample(tEaseIn)
+		scale.y = curve.sample(tEaseIn)
+		if tEaseIn >= curve.max_domain:
+			easing_in = false
 
 	# Flash the "next message" prompt
 	if flashing:		
